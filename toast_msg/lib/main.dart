6f7a4f9997a71bb99f16c22d68e7fae6f79608d7
Taehong Kim <public.devhong@gmail.com>
2021-10-20 20:13:21 +0900

    Toast message

---
+++ toast_msg/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Toast Message App",
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Toast App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        // ignore: deprecated_member_use
+        child: FlatButton(
+          child: const Text("Show me"),
+          onPressed: () {
+            flutterToast("Hello World");
+          },
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.redAccent,
+      textColor: Colors.white,
+      fontSize: 20.0);
+}

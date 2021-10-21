3e2545d665e61ed5d3075d53ced8ec4ac0e98cf5
Taehong Kim <public.devhong@gmail.com>
2021-10-21 21:12:57 +0900

    Update Snack Bar

--- snack_bar2/lib/main.dart
+++ snack_bar2/lib/main.dart
@@ -26,6 +26,22 @@ class MyPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: const MySnackBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: const Text("Like a new Snack bar!"),
+            duration: const Duration(seconds: 3),
+            action: SnackBarAction(
+              label: "Undo",
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ThridPage()));
+              },
+            ),
+          ));
+        },
+        child: const Icon(Icons.thumb_up),
+      ),
     );
   }
 }
@@ -36,22 +52,58 @@ class MySnackBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      // ignore: deprecated_member_use
-      child: RaisedButton(
-        child: const Text("Show me"),
-        onPressed: () {
-          Scaffold.of(context)
-              // ignore: deprecated_member_use
-              .showSnackBar(const SnackBar(
-            content: Text(
-              "Hello World",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
-            ),
-            backgroundColor: Colors.teal,
-            duration: Duration(milliseconds: 1000),
-          ));
-        },
+        child: ElevatedButton(
+      child: const Text("Go to the second page"),
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const SecondPage()));
+      },
+    ));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Page"),
+      ),
+      body: Builder(builder: (context) {
+        return Center(
+          child: Column(children: [
+            const Text("Canceled like.",
+                style: TextStyle(fontSize: 16.0, color: Colors.red)),
+            ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("좋아요가 취소되었습니다.")));
+                },
+                child: const Text("Cancel"))
+          ]),
+        );
+      }),
+    ));
+  }
+}
+
+class ThridPage extends StatelessWidget {
+  const ThridPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Thrid Page"),
+      ),
+      body: const Center(
+        child: Text(
+          "3번째 페이지",
+          style: TextStyle(fontSize: 16.0, color: Colors.red),
+        ),
       ),
     );
   }

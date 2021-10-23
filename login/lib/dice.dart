20bf680b996dc216cfb6d8ba9e208a4674da2ad1
Taehong Kim <public.devhong@gmail.com>
2021-10-23 11:54:08 +0900

    Dice Game

--- login/lib/dice.dart
+++ login/lib/dice.dart
@@ -1,7 +1,17 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +19,63 @@ class Dice extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dice Game"),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Image(
+                      image: AssetImage("images/dice$leftDice.png"),
+                      width: 150.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: Image(
+                      image: AssetImage("images/dice$rightDice.png"),
+                      width: 150.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                    rightDice = Random().nextInt(6) + 1;
+
+                    if (leftDice > rightDice) {
+                      int tmp = leftDice;
+                      leftDice = rightDice;
+                      rightDice = tmp;
+                    }
+                    showToast("Left Dice: $leftDice, Right Dice: $rightDice");
+                  });
+                },
+                child: const Icon(Icons.play_arrow))
+          ],
+        ),
+      ),
     );
   }
+}
+
+void showToast(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      fontSize: 14.0);
 }
--- login/lib/main.dart
+++ login/lib/main.dart
@@ -37,70 +37,76 @@ class _LoginState extends State<Login> {
           centerTitle: true,
         ),
         body: Builder(builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                const Padding(padding: EdgeInsets.only(top: 50)),
-                const Center(
-                  child: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      backgroundImage: AssetImage("images/iu.png"),
-                      radius: 50),
-                ),
-                Form(
-                  child: Theme(
-                      data: ThemeData(
-                          primaryColor: Colors.teal,
-                          inputDecorationTheme: const InputDecorationTheme(
-                              labelStyle: TextStyle(
-                                  color: Colors.teal, fontSize: 15.0))),
-                      child: Container(
-                        padding: const EdgeInsets.all(40.0),
-                        child: Column(
-                          children: <Widget>[
-                            TextField(
-                              decoration: const InputDecoration(
-                                  labelText: "Enter email"),
-                              keyboardType: TextInputType.emailAddress,
-                              controller: controller,
-                            ),
-                            TextField(
-                              decoration: const InputDecoration(
-                                  labelText: "Enter password"),
-                              keyboardType: TextInputType.text,
-                              obscureText: true,
-                              controller: controller2,
-                            ),
-                            const Padding(padding: EdgeInsets.only(top: 30)),
-                            ElevatedButton(
-                              onPressed: () {
-                                if (controller.text == "a" &&
-                                    controller2.text == "123") {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => Dice()));
-                                } else {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(const SnackBar(
-                                    content: Text(
-                                      "일치하지 않습니다.",
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    duration: Duration(seconds: 2),
-                                  ));
-                                }
-                              },
-                              child: const Text("Login"),
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.teal,
-                                  fixedSize: const Size(300, 40)),
-                            )
-                          ],
-                        ),
-                      )),
-                )
-              ],
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  const Padding(padding: EdgeInsets.only(top: 50)),
+                  const Center(
+                    child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage("images/iu.png"),
+                        radius: 50),
+                  ),
+                  Form(
+                    child: Theme(
+                        data: ThemeData(
+                            primaryColor: Colors.teal,
+                            inputDecorationTheme: const InputDecorationTheme(
+                                labelStyle: TextStyle(
+                                    color: Colors.teal, fontSize: 15.0))),
+                        child: Container(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Column(
+                            children: <Widget>[
+                              TextField(
+                                decoration: const InputDecoration(
+                                    labelText: "Enter email"),
+                                keyboardType: TextInputType.emailAddress,
+                                controller: controller,
+                                autofocus: true,
+                              ),
+                              TextField(
+                                decoration: const InputDecoration(
+                                    labelText: "Enter password"),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                                controller: controller2,
+                              ),
+                              const Padding(padding: EdgeInsets.only(top: 30)),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (controller.text == "a" &&
+                                      controller2.text == "123") {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Dice()));
+                                  } else {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                      content: Text(
+                                        "일치하지 않습니다.",
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      duration: Duration(seconds: 2),
+                                    ));
+                                  }
+                                },
+                                child: const Text("Login"),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.teal,
+                                    fixedSize: const Size(300, 40)),
+                              )
+                            ],
+                          ),
+                        )),
+                  )
+                ],
+              ),
             ),
           );
         }),
